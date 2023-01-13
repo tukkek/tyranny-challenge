66146e988095483638f02cdd5439639e639b5546
Alex Henry <tukkek@gmail.com>
2023-01-13 01:33:55 -0300

    Permalink

--- script/rpg.js
+++ script/rpg.js
@@ -1,3 +1,7 @@
+const LOCATION=new URL(document.location)
+
+export var seed=LOCATION.searchParams.get('seed')||Date.now()
+
 export function roll(min,max){return Math.floor(Math.random()*(max-min+1))+min}
 
 export function pick(array){return array[roll(0,array.length-1)]}
@@ -12,4 +16,13 @@ export function shuffle(array){
     array[j]=a
   }
   return array
+}
+
+export function setup(){
+  Math.seedrandom(seed.toString())
+  let u=LOCATION
+  u.searchParams.set('seed',seed)
+  document.querySelector('#permalink').href=u
+  u.searchParams.delete('seed')
+  document.querySelector('#new').href=u
 }
