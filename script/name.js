c96bfa0adcb80d9d2cb81b7bbb79857dc239e117
Alex Henry <tukkek@gmail.com>
2023-06-29 00:00:00 +0000

    Fix names JSON path

--- script/name.js
+++ script/name.js
@@ -1,7 +1,7 @@
 export var names=[]
 
 export async function setup(){
-  names=await fetch('../neutralNames.json')
+  names=await fetch('neutralNames.json')
   names=await names.json()
   names=names['neutralNames']
 }
